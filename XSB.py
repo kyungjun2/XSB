909128ca2f34908aba35056298802b6f759ce609
Kyungjun <kyungjun2@gmail.com>
2020-03-30 20:12:38 +0900

    [20.3.30] 로직 개선

---
+++ XSB.py
@@ -0,0 +1,223 @@
+class XSBError(BaseException):
+    def __init__(self, message):
+        self.message = message
+
+
+class XSB:
+    def __init__(self):
+        self.target = None
+        self.credential = {}
+        self.blog_name = {}
+
+        self.target_posts = []
+        self.downloaded_posts = []
+        self.results = []
+
+    def validate_stage(self, level, posts=None):
+        # 1단계 - 로그인
+        try:
+            temp = self.credential['naver']
+            temp = self.credential['tistory']
+        except KeyError:
+            raise XSBError("잘못된 접근입니다.")
+
+        # 2단계 - 목적지
+        if self.target == 'naver':
+            pass
+        elif self.target == 'tistory':
+            pass
+        else:
+            raise XSBError("잘못된 접근입니다.")
+
+        if level == 3:  # 3단계 앞의 과정을 거쳤는지만 확인
+            return
+
+        # 3단계 - 최근 글 선택
+        if not self.target_posts:
+            raise XSBError("잘못된 접근입니다.")
+
+        if level == 4:  # 4단계 앞의 과정을 거쳤는지만 확인
+            return
+
+        # 4단계 - 글 다운로드
+        if not self.downloaded_posts:
+            raise XSBError("잘못된 접근입니다.")
+
+        if level == 5:  # 5단계 앞의 과정을 거쳤는지만 확인
+            return
+
+    def recent_post(self, target):
+        self.validate_stage(level=3)
+        if target == 'naver':
+            import requests
+            import xml.etree.ElementTree as ET
+
+            posts = []
+            url = "https://rss.blog.naver.com/{}.xml".format(self.blog_name['naver'])
+            x = ET.fromstring(requests.get(url).text)
+
+            for item in x.findall('./channel/item'):
+                post = {}
+                for child in item:
+                    if child.tag == "title":
+                        post['title'] = child.text
+                    elif child.tag == "link":
+                        post['postLink'] = child.text
+                        post['postId'] = child.text.split('/')[-1]
+                    elif child.tag == "pubDate":
+                        post['writeDate'] = child.text
+                posts.append(post)
+            return posts
+
+        elif target == 'tistory':
+            import json
+            import requests
+
+            url = "https://www.tistory.com/apis/post/list"
+            param = {'access_token': self.credential['tistory'], 'output': 'json',
+                     'blogName': self.blog_name['tistory'], 'page': '1'}
+            j = json.loads(requests.get(url, params=param).text)
+
+            posts = j['tistory']['item']['posts']
+            ret = []
+            for post in posts:
+                ret.append(
+                    {'postLink': str(post['postUrl']), 'title': str(post['title']), 'writeDate': str(post['date']),
+                     'postId': str(post['postUrl']).split('/')[-1]})
+            return ret
+        else:
+            raise XSBError("잘못된 접근입니다.")
+
+    def download_post(self, post_list):
+        import json
+        import requests
+        from bs4 import BeautifulSoup as bs
+        import re
+        import config
+        import platform
+        from pathlib import Path
+        self.validate_stage(level=4)
+
+        if self.target == 'naver':
+            def download_tistory(postid):
+                # 1. 요청
+                url = "https://www.tistory.com/apis/post/read"
+                param = {'access_token': self.credential['tistory'], 'blogName': self.blog_name['tistory'],
+                         'postId': postid, 'output': 'json'}
+                j = json.loads(requests.get(url=url, params=param).text)
+
+                # 2. 파싱
+                # 2-1. 이미지 다운로드
+                r = re.compile("""\[##_Image\|kage@([a-zA-Z0-9/.]+).+_##\]""")
+                r2 = re.compile("""(\[##_Image\|kage@[a-zA-Z0-9/.]+.+_##\])""")
+                content = j['tistory']['item']['content']
+
+                img_urls = []
+                idx = 0
+                image_data = {}
+
+                path = config.file_save_path + "{0}\\{1}\\{2}\\".format(self.target, self.blog_name['tistory'], postid)\
+                    if platform.system() == "Windows" \
+                    else "{0}/{1}/{2}/".format(self.target, self.blog_name['tistory'], postid)
+                Path(path).mkdir(parents=True, exist_ok=True)
+
+                for img in r.findall(content):
+                    req = requests.get("https://k.kakaocdn.net/dn/" + img, allow_redirects=True)
+                    open(path + str(idx) + "." + img.split('.')[-1], 'wb').write(req.content)
+
+                    img_urls.append("https://k.kakaocdn.net/dn/" + img)
+                    image_data[str(idx) + "." + url.split('.')[-1]] = req.headers['content-type']
+                    img_urls.append(url)
+                    idx += 1
+                with open(path + 'images.json', 'w') as file:
+                    json.dump(image_data, file)
+
+                # 2-2. 링크 교체
+                content = r2.sub("""<img src="">""", content)
+                soup = bs(content, 'html.parser')
+                idx = 0
+                for img in soup.find_all("img"):
+                    img.attrs['source'] = img_urls[idx]
+                    idx += 1
+
+                # 3. 결과 리턴
+                ret = {'title': j['tistory']['item']['title'], 'content': content,
+                       'writeDate': j['tistory']['item']['date'], 'postUrl': j['tistory']['item']['postUrl']}
+                if len(img_urls) != 0:
+                    ret['image_path'] = path
+                return ret
+
+            results = []
+            for post in post_list:
+                results.append(download_tistory(post))
+            self.downloaded_posts = results
+            return results
+
+        elif self.target == 'tistory':
+            def download_naver(postid):
+                # 1. 요청
+                url = "https://blog.naver.com/PostView.nhn?blogId={0}&logNo={1}".format(self.blog_name['naver'], postid)
+                r = requests.get(url=url)
+                soup = bs(r.text, "html.parser")
+
+                # 2. 파싱
+                # 2-1. 글 내용 파싱
+                try:  # 최신버전 smartEditor 대응
+                    content = str(soup.select_one("table#printPost1 div.se-main-container").prettify())
+                    title = soup.select_one("table#printPost1 div.se-title-text p").text
+                    writeDate = soup.select_one("table#printPost1 span.se_publishDate").text
+                except (KeyError, AttributeError):
+                    try:  # 옛버전 smartEditor 대응
+                        title = soup.select_one("div.htitle span").text
+                        writeDate = soup.select_one("p.date").text
+                        content = str(soup.select_one("div#postViewArea").prettify())
+                    except (KeyError, AttributeError):  # API로 쓴 글 대응
+                        content = str(soup.select("table#printPost1 div.se_component_wrap")[1].prettify())
+                        title = soup.select_one("table#printPost1 div.se_title h3").text
+                        writeDate = soup.select_one("table#printPost1 span.se_publishDate").text
+
+                # 2-2. 이미지 다운로드
+                idx = 0
+                img_urls = []
+                image_data = {}
+
+                soup = bs(content, "html.parser")
+                path = config.file_save_path + "{0}\\{1}\\{2}\\".format(self.target, self.blog_name['naver'], postid) \
+                    if platform.system() == "Windows" \
+                    else "{0}/{1}/{2}/".format(self.target, self.blog_name['naver'], postid)
+                Path(path).mkdir(parents=True, exist_ok=True)
+
+                for img in soup.find_all("img"):
+                    url = img.attrs['src'] if img.attrs['src'] is not None else img.attrs['data-lazy-src']
+                    url = url.split('?type')[0]
+                    req = requests.get(url + ("?type=w1" if url.count("postfiles") != 0 else ""), allow_redirects=True)
+                    open(path + str(idx) + "." + url.split('.')[-1], 'wb').write(req.content)
+
+                    image_data[str(idx) + "." + url.split('.')[-1]] = req.headers['content-type']
+                    img_urls.append(url)
+                    idx += 1
+                with open(path + 'images.json', 'w') as file:
+                    json.dump(image_data, file)
+
+                # 3. 결과 리턴
+                ret = {'title': title, 'content': content, 'writeDate': writeDate, 'postUrl': url}
+                if len(img_urls) != 0:
+                    ret['image_path'] = path
+                return ret
+
+            results = []
+            for post in post_list:
+                results.append(download_naver(post))
+            self.downloaded_posts = results
+            return results
+        else:
+            raise XSBError("잘못된 접근입니다.")
+
+    def upload_post(self):
+        self.validate_stage(level=5)
+        if self.target == 'naver':
+            pass
+        elif self.target == 'tistory':
+            pass
+        else:
+            raise XSBError("잘못된 접근입니다.")
---
+++ main.py
@@ -0,0 +1,151 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(16)
+
+
+@app.route('/')
+def welcome():
+    from flask import render_template
+    return render_template('index.html')
+
+
+@app.route('/xsb', methods=['GET', 'POST'])
+def main():
+    from flask import session, render_template, request
+    from XSB import XSB, XSBError
+
+    xsb = XSB()
+    try:
+        # 이전에 만들어둔 값이 있으면 로그인 과정 무시
+        xsb.__dict__ = session['xsb']
+    except KeyError:
+        # 1. 로그인
+        try:
+            xsb.credential['naver'] = session['naver_token']
+            xsb.blog_name['naver'] = session['naver_name']
+        except KeyError:
+            return render_template('login.html', target='naver')
+        try:
+            xsb.credential['tistory'] = session['tistory_token']
+            xsb.blog_name['tistory'] = session['tistory_name']
+        except KeyError:
+            return render_template('login.html', target='tistory')
+
+    # 2. 어디로 글을 옮길건지 선택
+    if xsb.target is None and request.args.get('target') is None:
+        return render_template('select_target.html')
+    elif xsb.target is None and request.args.get('target') is not None:
+        xsb.target = request.args.get('target')
+    session['xsb'] = xsb.__dict__
+
+    # 3. 최근 글 파싱
+    if request.args.get("stage") is None:
+        if xsb.target == 'naver':
+            recent = xsb.recent_post(target='tistory')
+            return render_template("recent_articles.html", posts=recent)
+        elif xsb.target == 'tistory':
+            recent = xsb.recent_post(target='naver')
+            return render_template("recent_articles.html", posts=recent)
+        else:
+            del session['xsb']
+            return "잘못된 접근입니다."
+
+    # 4. 선택된 글 다운로드
+    elif request.args.get("stage") == "4":
+        selected_posts = request.form.getlist('selection[]')
+        xsb.target_posts = selected_posts
+        download_result = xsb.download_post(selected_posts)
+        session['xsb'] = xsb.__dict__
+        return render_template("confirm_upload.html", posts=download_result,
+                               blog={'source': '티스토리' if xsb.target == 'naver' else '네이버',
+                                     'target': '티스토리' if xsb.target == 'tistory' else '네이버'})
+    # 5. 글 업로드
+    elif request.args.get("stage") == "5":
+        raise NotImplementedError
+
+    else:
+        return "잘못된 접근입니다."
+
+    # 6. 모든 작업이 끝나면 오브젝트 삭제
+    del session['xsb']
+    del xsb
+
+
+@app.route('/login', methods=['GET'])
+def login_requests():
+    from flask import request, redirect
+    import requests
+    import keys
+
+    if request.args.get("target") is None or request.args.get("blogName") is None:
+        return "잘못된 접근입니다."
+
+    if request.args.get("target") == "naver":
+        url = "https://nid.naver.com/oauth2.0/authorize"
+        param = {'client_id': keys.naver_app_id, 'redirect_uri': keys.naver_callback_uri, 'response_type': 'code',
+                 'state': request.args.get("blogName")}
+        return redirect(requests.get(url, params=param).url, code=302)
+    elif request.args.get("target") == "tistory":
+        url = "https://www.tistory.com/oauth/authorize"
+        param = {'client_id': keys.tistory_app_id, 'redirect_uri': keys.tistory_callback_uri, 'response_type': 'code',
+                 'state': request.args.get("blogName")}
+        return redirect(requests.get(url, params=param).url, code=302)
+    else:
+        return "잘못된 접근입니다."
+
+
+@app.route('/tistory_auth_code', methods=['GET'])
+def tistory_access_token():
+    import keys
+    import requests
+    from urllib import parse
+    from flask import redirect, url_for, request, session
+
+    try:
+        url = "https://www.tistory.com/oauth/access_token"
+        param = {'client_id': keys.tistory_app_id, 'redirect_uri': keys.tistory_callback_uri,
+                 'grant_type': 'authorization_code', 'client_secret': keys.tistory_secret,
+                 'code': request.args.get('code')}
+        r = requests.get(url, params=param)
+        if r.text.count("invalid_request") is not 0:
+            raise IndexError
+
+        token = r.text.split('=')[1]
+        blog_name = parse.urlparse(request.args.get('state'))[2]
+
+        session['tistory_token'] = token
+        session['tistory_name'] = blog_name
+        return redirect(url_for('main'))
+
+    except (IndexError, ValueError):
+        return "로그인 실패"
+
+
+@app.route('/naver_auth_code', methods=['GET'])
+def naver_access_token():
+    import keys
+    import requests
+    import json
+    from urllib import parse
+    from flask import redirect, url_for, request, session
+
+    try:
+        url = "	https://nid.naver.com/oauth2.0/token"
+        param = {'client_id': keys.naver_app_id, 'redirect_uri': keys.naver_callback_uri,
+                 'grant_type': 'authorization_code', 'client_secret': keys.naver_secret,
+                 'code': request.args.get('code'), 'state': request.args.get('state')}
+        r = json.loads(requests.get(url, params=param).text)
+
+        session['naver_name'] = parse.urlparse(request.args.get('state'))[2]
+        session['naver_token'] = r['access_token']
+
+        return redirect(url_for('main'))
+    except KeyError:
+        return "로그인 실패"
+
+
+####  개발용  ####
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=12321, debug=True)
